a4ff7ba252ea1610ab5c7a116e78b52b3e3ddc2a
musarraf23 <musarrafmansoori1025@gmail.com>
2024-08-10 00:56:14 +0530

    Home Page Modified

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useState } from 'react';
+import HighlightText from './HighlightText';
+import { HomePageExplore } from "../../../data/homepage-explore";
 
+
+const tabsName = [
+    "Free",
+    "New to coding",
+    "Most popular",
+    "Skills paths",
+    "Career paths",
+];
 const ExploreMore = () => {
+    const [currentTab , setCurrentTab ] = useState(tabsName[0]);
+    const [courses , setCourses] = useState()
   return (
     <div>
       
